59c7fada1c141e4046916b372620cb2da9628685
Khush252 <khush.bajaj123@gmail.com>
2024-06-13 19:05:32 +0530

    created singleton class

--- src/database.py
+++ src/database.py
@@ -2,9 +2,20 @@ from pymongo import MongoClient
 import config
 
 class MongoDBProfiler:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._initialized = False
+        return cls._instance
+
     def __init__(self):
+        if self._initialized:
+            return
         self.client = MongoClient(config.MONGODB_CONNECTION_STRING)
         self.db = self.client[config.DATABASE_NAME]
+        self._initialized = True
 
     def enable_profiling(self):
         self.db.command("profile", 1, slowms=config.SLOWMS)
