23f67f46fa8fd5090cb11c036845ee65d9cc0855
Khush252 <khush.bajaj123@gmail.com>
2024-07-02 19:02:11 +0530

    bytes sorted

--- utils/serialize_csv.py
+++ utils/serialize_csv.py
@@ -1,4 +1,4 @@
-from bson import Binary, ObjectId, Timestamp, json_util
+from bson import Binary, ObjectId, json_util
 
 # Serialization because csv supports only particular data types
 def serialize_for_csv(data):
@@ -6,11 +6,11 @@ def serialize_for_csv(data):
         return data.hex()  # Convert Binary to hex string
     elif isinstance(data, ObjectId):
         return str(data)  # Convert ObjectId to string
-    elif isinstance(data, Timestamp):
-        return str(data)  # Convert Timestamp to string
     elif isinstance(data, dict):
         return {k: serialize_for_csv(v) for k, v in data.items()}
     elif isinstance(data, list):
         return [serialize_for_csv(item) for item in data]
+    elif isinstance(data, bytes):
+        return data.decode('utf-8', errors='replace')  # Convert bytes to string
     else:
-        return str(data)
+        return data
